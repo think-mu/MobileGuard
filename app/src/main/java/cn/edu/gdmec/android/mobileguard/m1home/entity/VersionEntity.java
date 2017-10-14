4a5546c07b23faf47703235779b2269c3cc34f9f
think-mu <1138793164@qq.com>
2017-10-14 16:32:23 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/entity/VersionEntity.java
@@ -0,0 +1,11 @@
+package cn.edu.gdmec.android.mobileguard.m1home.entity;
+
+/**
+ * Created by Hwangzbin on 2017/9/28.
+ */
+
+public class VersionEntity {
+    public String versioncode;
+    public String description;
+    public String apkurl;
+}
