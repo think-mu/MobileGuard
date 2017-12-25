761502ee1c503eb348bd358ae289f0bebad079ac
JohnChenNA <605161453@qq.com>
2017-12-25 14:21:49 +0800

    调回升级弹窗

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/SplashActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/SplashActivity.java
@@ -3,6 +3,7 @@ package cn.edu.gdmec.android.mobileguard;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 import android.widget.TextView;
 
 import cn.edu.gdmec.android.mobileguard.m1home.HomeActivity;
@@ -25,12 +26,26 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_splash);
         mVersion = MyUtils.getVersion(getApplicationContext());
         mTvVersion = (TextView) findViewById(R.id.tv_splash_version);
         mTvVersion.setText("版本号:"+mVersion);
-
-        startActivity(new Intent(this, HomeActivity.class));
-        finish();
+        VersionUpdateUtils.DownloadCallback downloadCallback = new VersionUpdateUtils.DownloadCallback(){
+            @Override
+            public void afterDownload(String filename){
+                MyUtils.installApk(SplashActivity.this,filename);
+            }
+        };
+        final VersionUpdateUtils versionUpdateUtils= new VersionUpdateUtils(mVersion,SplashActivity.this,downloadCallback,HomeActivity.class);
+        new Thread(){
+            @Override
+            public void run(){
+                super.run();
+                versionUpdateUtils.getCloudVersion("http://android2017.duapp.com/updateinfo.html");
+            }
+        }.start();
+//        startActivity(new Intent(this, HomeActivity.class));
+//        finish();
     }
 }
