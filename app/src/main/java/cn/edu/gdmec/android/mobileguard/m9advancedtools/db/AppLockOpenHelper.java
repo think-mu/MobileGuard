a699ac5676106c4e7e83c33ccd4c6b865f1c7962
think-mu <1138793164@qq.com>
2017-12-26 00:14:22 +0800

    号码查询

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m9advancedtools/db/AppLockOpenHelper.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m9advancedtools/db/AppLockOpenHelper.java
@@ -1,25 +1,22 @@
+
 package cn.edu.gdmec.android.mobileguard.m9advancedtools.db;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-/**
- * Created by admin on 2017/12/23.
- */
-
 public class AppLockOpenHelper extends SQLiteOpenHelper {
-    public AppLockOpenHelper(Context context){
-        super(context,"applock.db",null,1);
+    public AppLockOpenHelper(Context context) {
+        super(context, "applock.db", null, 1);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase db){
-        db.execSQL("create table applock (id integer primary key autoincrement,packagename varchar(20))");
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table applock (id integer primary key autoincrement, packagename  varchar(20))");
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
     }
 }
